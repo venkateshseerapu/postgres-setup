7a5aecf28a5a44585e176ad9bd819ca22b56ba2f
venkateshseerapu <venkatesh.seerapu@gmail.com>
2014-04-04 18:00:19 +0530

    added postgres setup files

---
+++ chef/cookbooks/postgres-db-setup/attributes/default.rb
@@ -0,0 +1,14 @@
+default[:home] = "/home/sqat"
+
+default[:system][:user] = ""
+default[:system][:owner] = ""
+
+default[:postgres][:software] = "postgresql-9.1"
+default[:postgres][:workarea] = "/opt/postgresql"
+default[:postgres][:version] = "/9.1"
+default[:postgres][:data] = "/data"
+default[:postgres][:username] = "sqat"
+default[:postgres][:dbname] = "sqatdb"
+default[:postgres][:password] = "password"
+
+
---
+++ chef/cookbooks/postgres-db-setup/solo.rb~
@@ -0,0 +1,3 @@
+  file_cache_path "/home/bhagyalakshmi/Temp"
+  cookbook_path "/home/bhagyalakshmi/chef/cookbooks"
+  json_attribs "/home/bhagyalakshmi/chef/cookbooks/postgres-db-setup/node.json"
