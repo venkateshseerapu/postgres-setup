105869cde4e6b7f0ca0c94cd25437312315f9c1c
venkateshseerapu <venkatesh.seerapu@gmail.com>
2014-04-04 18:11:31 +0530

    Update default.rb
    Modified attributes

--- chef/cookbooks/postgres-db-setup/attributes/default.rb
+++ chef/cookbooks/postgres-db-setup/attributes/default.rb
@@ -1,7 +1,7 @@
-default[:home] = "/home/sqat"
+default[:home][:workarea] = "/home/bhagyalakshmi"
 
 default[:system][:user] = ""
-default[:system][:owner] = ""
+default[:system][:owner] = "root"
 
 default[:postgres][:software] = "postgresql-9.1"
 default[:postgres][:workarea] = "/opt/postgresql"
